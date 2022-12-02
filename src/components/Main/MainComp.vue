8ac2c3605f43e9d52642c695a42a82aab4691c34
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-12-02 18:37:02 +0100

    sezione Our Products completa

---
+++ src/components/Main/MainComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <OurProducts/>
+    </div>
+</template>
+
+<script>
+import OurProducts from '../Main/OurProducts.vue'
+    export default {
+        name: 'MainComp',
+        components: {
+            OurProducts,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main/OurProducts.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="ourProducts">
+    <div class="ourProductsContainer">
+      <div class="prodDescription">
+        <p class="fresh">Our Products</p>
+        <h1>
+          All our delectable pastries are backed fresh in our Kitchen every
+          morning and are made with all-natural, all-organic ingredients.
+        </h1>
+        <button>Start Shopping</button>
+      </div>
+      <div class="prodSlider">
+        <div class="leftSlider">
+            <img src="../../assets/img/choco-chip-cookies-768x979.jpg" alt="">
+            <div class="abL"> <strong>&lt;</strong> </div>
+        </div>
+        <div class="rightSlider">
+            <img src="../../assets/img/strawberry-jam-cookies-768x979.jpg" alt="">
+            <div class="abR"> <strong>&gt;</strong> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OurProducts",
+};
+</script>
+
+<style lang="scss" scoped>
+
+.ourProducts{
+    width: 100%;
+    margin-top: 6rem;
+    margin-bottom: 10rem;
+}
+
+.ourProductsContainer{
+    width: 90%;
+    margin: auto;
+    display: flex;
+}
+
+
+
+.prodSlider{
+    display: flex;
+    justify-content: center;
+    gap: 25px;
+}
+
+.prodDescription{
+    width: 30%;
+    
+}
+
+.fresh {
+  font-size: 0.7rem;
+}
+
+.leftSlider{
+    width: 40%;
+    position: relative;
+    img{
+        width: 100%;
+    }
+}
+
+.rightSlider{
+    width: 40%;
+    position: relative;
+    img{
+        width: 100%;
+    }
+}
+
+.abR{
+    background-color: #5b2ca2;
+    padding: 2rem 1rem;
+    position: absolute;
+    top: 45%;
+    right: 0;
+    color: white;
+    opacity: 0.6;
+}
+
+.abL{
+    background-color: #5b2ca2;
+    padding: 2rem 1rem;
+    position: absolute;
+    top: 45%;
+    color: white;
+    opacity: 0.6;
+}
+
+.abL:hover{
+    background-color: gray
+}
+
+.abR:hover{
+    background-color: gray
+}
+
+h1 {
+  font-size: 2.2rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  color: #5b2ca2;
+  text-align:start;
+//   padding-right: 0.5rem;
+}
+
+button {
+  margin-top: 1rem;
+  padding: 1rem;
+  color: white;
+  border-color: #5b2ca2;
+  background-color: #5b2ca2;
+  border-style: none;
+  border-radius: 5px;
+}
+
+button:hover {
+  background-color: white;
+  color: #5b2ca2;
+  border: 2px solid grey
+}
+</style>
